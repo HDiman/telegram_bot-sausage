342de17885127d0e33e2e296574fd39eb4588a68
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-06-20 00:22:34 +0300

    Working with information

--- main.py
+++ main.py
@@ -3,45 +3,19 @@ from bs4 import BeautifulSoup
 import telebot
 from auth_data import token
 
+url_1 = "https://edadeal.ru/moskva/offers?retailer=5ka&retailer=ashan&retailer=auchan&retailer=auchan-city&retailer=dixy&retailer=globus&retailer=magnit-giper&retailer=magnit-univer&retailer=market-da&retailer=mgnl&retailer=miasnitskii_riad&retailer=perekrestok&retailer=verno&retailer=victoria&retailer=vkusvill_offline&segment=sausages&sort=aprice"
+url_2 = "https://edadeal.ru/moskva/offers?page=2&retailer=5ka&retailer=ashan&retailer=auchan&retailer=auchan-city&retailer=dixy&retailer=globus&retailer=magnit-giper&retailer=magnit-univer&retailer=market-da&retailer=mgnl&retailer=miasnitskii_riad&retailer=perekrestok&retailer=verno&retailer=victoria&retailer=vkusvill_offline&segment=sausages&sort=aprice"
+url_3 = "https://edadeal.ru/moskva/retailers/5ka"
 
-def get_data():
-    url_parsing = ""
-    headers = {
-        "Accept": "*/*",
-        "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
-    }
+headers = {
+    "Accept": "*/*",
+    "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+}
 
-    # --- Block to get data ---
-    req = requests.get(url=url_parsing, headers=headers)
-    response = req.text
+# --- Block for use only ones ---
+req = requests.get(url=url_3, headers=headers)
+response = req.text
+print(response)
 
-    soup = BeautifulSoup(response, "lxml")
-    all_title = soup.find_all(class_="g-user-content")
-    return all_title
-
-
-def telegram_bot(token):
-    bot = telebot.TeleBot(token)
-
-    @bot.message_handler(commands=['start'])
-    def start_message(message):
-        bot.send_message(message.chat.id, "Code to solve problems")
-
-    @bot.message_handler(content_types=['text'])
-    def send_text(message):
-        if message.text.lower() == 'hh':
-            try:
-                all_title = get_data()
-                for title in all_title:
-                    bot.send_message(message.chat.id, title.text)
-            except Exception as ex:
-                print(ex)
-                bot.send_message(message.chat.id, "Disconnection!!!")
-        else:
-            bot.send_message(message.chat.id, "More coding")
-
-    bot.polling()
-
-
-if __name__ == "__main__":
-    telegram_bot(token=token)
+with open("index.html", "w") as file:
+    file.write(response)
