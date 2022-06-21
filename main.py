60f2ef76197d620e77694e6ef00d2fa7c6896064
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-06-21 14:27:44 +0300

    Quotes ready for use un telegram chat

--- main.py
+++ main.py
@@ -6,21 +6,32 @@ from auth_data import token
 
 
 def get_data():
-    url = "https://www.forbes.ru/forbeslife/dosug/262327-na-vse-vremena-100-vdokhnovlyayushchikh-tsitat"
+    rnd = random.randint(1, 26) # page number
+    url = f"https://bbf.ru/quotes/?page={rnd}&tag=73"
     headers = {
         "Accept": "*/*",
         "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
     }
-    list = []
+    list_quotes = []
+    list_authors = []
+
     # --- Block to get data ---
     req = requests.get(url=url, headers=headers)
     response = req.text
 
     soup = BeautifulSoup(response, "lxml")
-    context = soup.find_all(class_="yl27R")
-    for item in context:
-        list.append(item.text)
-    return list
+    quotes = soup.find_all(class_="sentence__body")
+    authors = soup.find_all(class_="sentence__author")
+    for quote in quotes:
+        list_quotes.append(quote.text)
+    for author in authors:
+        list_authors.append(author.text)
+    dict_quotes = dict(zip(list_quotes, list_authors))
+
+    # for key, value in dict_quotes.items():
+    #     print(f"{key}{value}")
+    return dict_quotes
+
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
@@ -33,9 +44,8 @@ def telegram_bot(token):
     def send_text(message):
         if message.text.lower() == 'g':
             try:
-                rnd = random.randrange(0, 100, 2)
-                text_data = get_data()[rnd] + " " + "(" + get_data()[rnd+1] + ")"
-                bot.send_message(message.chat.id, text_data)
+                quote, author = random.choice(list(get_data().items()))
+                bot.send_message(message.chat.id, f"{quote}{author}")
             except Exception as ex:
                 print(ex)
                 bot.send_message(message.chat.id, "Error!!!")
@@ -46,8 +56,6 @@ def telegram_bot(token):
 
 
 if __name__ == "__main__":
+    # get_data()
     telegram_bot(token=token)
-    # list = get_data()
-    # for _ in range(200):
-    #     rnd = random.randrange(0, 200, 2)
-    #     print(list[rnd] + " " + "(" + list[rnd+1] + ")")
+
